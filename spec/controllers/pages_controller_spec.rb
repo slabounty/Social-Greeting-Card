b5271019dff1eac0fc21b04a7e2516e77bb49a01
slabounty <slabounty@gmail.com>
2011-12-11 18:36:54 -0800

    Added help page and tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -40,4 +40,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+        get 'help'
+        response.should have_selector("title", :content => "Social Greeting | Help")
+    end
+  end
+
 end
