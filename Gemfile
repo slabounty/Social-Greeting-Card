217b469fb88fb66584c138b2bc25ffb1af664ed6
slabounty <slabounty@gmail.com>
2011-12-13 21:37:55 -0800

    User model with passwords and user show page

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
-
+gem 'gravatar_image_tag', '0.1.0'
 gem 'jquery-rails'
 
 group :development do
@@ -13,6 +13,7 @@ group :test do
   gem 'sqlite3'
   gem 'webrat', '0.7.1'
   gem 'rspec', '2.6'
+  gem 'factory_girl_rails', '1.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+# User symbol :user get Factory Girl to simulate teh User model.
+Factory.define :user do |user|
+    user.first_name             "Scott"
+    user.last_name              "LaBounty"
+    user.email                  "slabounty@socialgreeting.com"
+    user.password               "foobar"
+    user.password_confirmation  "foobar"
+end
