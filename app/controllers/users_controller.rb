c73bbe7981835be88f790d8ae13d283d900be5e9
slabounty <slabounty@gmail.com>
2011-12-14 13:01:42 -0800

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
       @title = "Sign up"
+      @user = User.new
   end
 
   def show
       @user = User.find(params[:id])
       @title = "#{@user.first_name} #{@user.last_name}"
+  end
+
+  def create
+      @user = User.new(params[:user])
+      if @user.save
+          flash[:success] = "Welcome to Greeting Social!"
+          redirect_to @user
+      else
+          @title = "Sign up"
+          render 'new'
+      end
   end
 
 end
