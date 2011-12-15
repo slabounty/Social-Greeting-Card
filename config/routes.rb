e928c538ce75889a5ab731e8cf5407d765fb3205
slabounty <slabounty@gmail.com>
2011-12-15 10:47:21 -0800

    Done with sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Greeting::Application.routes.draw do
 
     resources :users
+    resources :sessions, :only => [:new, :create, :destroy]
 
     match '/signup', :to => 'users#new'
+    match '/signin', :to => 'sessions#new'
+    match '/signout', :to => 'sessions#destroy'
 
     root :to => 'pages#home'
     match '/contact', :to => 'pages#contact'
