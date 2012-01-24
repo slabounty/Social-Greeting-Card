de22656492139d133e3aafecdadaf6e2511371d3
slabounty <slabounty@gmail.com>
2012-01-23 19:34:55 -0800

    Moved the email from application_helper to application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  include ApplicationHelper
-  include SessionsHelper
+
+    protect_from_forgery
+
+    include SessionsHelper
+
+    def email(to, subject, body)
+        Pony.mail(:to => to, :via => :smtp, :via_options => 
+                  {
+                      :address => 'smtp.gmail.com',
+                      :port => '587',
+                      :enable_starttls_auto => true,
+                      :user_name => 'greetingsocial',
+                      :password => 's0cI41greeting',
+                      :authentication => :plain,
+                      :domain => "HELO",
+                  },
+                  :subject => subject,
+                  :body => body)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,18 +12,4 @@ module ApplicationHelper
         image_tag("logo.png", :alt => "Greeting Social", :class => "round")
     end
 
-    def email(to, subject, body)
-        Pony.mail(:to => to, :via => :smtp, :via_options => 
-                  {
-                      :address => 'smtp.gmail.com',
-                      :port => '587',
-                      :enable_starttls_auto => true,
-                      :user_name => 'greetingsocial',
-                      :password => 's0cI41greeting',
-                      :authentication => :plain,
-                      :domain => "HELO",
-                  },
-                  :subject => subject,
-                  :body => body)
-    end
 end
